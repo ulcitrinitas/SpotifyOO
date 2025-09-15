19a8dc06888cb6bb0eabefd14e52dd95df00d33f
ulcitrinitas <uli.citrinitas@gmail.com>
2025-09-15 18:49:30 -0300

    Criado audiolivro

---
+++ src/Audiolivro.java
@@ -0,0 +1,17 @@
+public class Audiolivro extends Musica {
+
+    String descricao;
+
+    Audiolivro(String nome, String author, String genre, String duracao, String lancamento) {
+        super(nome, author, genre, duracao, lancamento);
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
