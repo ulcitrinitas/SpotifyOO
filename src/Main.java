f0b23eae02ce065de24a7c8a96af1ad83f8399f8
ulcitrinitas <uli.citrinitas@gmail.com>
2025-09-15 16:46:34 -0300

    Criado claculo pra converter minutos em milisegundos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi, Java!");
+
+        var dur = Musica.calculoDuracao("1:30");
+
+        System.out.println(dur);
+
+
     }
 }
--- src/Musica.java
+++ src/Musica.java
@@ -16,4 +16,18 @@ public class Musica {
         this.genre = genre;
     }
 
+    static public long calculoDuracao(String tempo){
+        var tempos = tempo.split(":");
+
+        if(tempos.length == 2){
+            var min =  Long.parseLong(tempos[0]);
+            var seg = Long.parseLong(tempos[1]);
+
+            seg = seg + (min*60);
+
+            return seg ;
+        }
+        return 0L;
+    }
+
 }
