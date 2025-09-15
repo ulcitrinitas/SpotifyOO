0b460b4391ba79b478e91b7e2b11b604e96ff655
ulcitrinitas <uli.citrinitas@gmail.com>
2025-09-15 18:10:54 -0300

    cria o arquivo de descrição

--- src/EpPodcast.java
+++ src/EpPodcast.java
@@ -1,8 +1,11 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class EpPodcast extends Musica {
 
     HashMap<String, Double> votos;
+    String descrição;
 
     EpPodcast(String nome, String author, String genre, String duracao, String lancamento) {
         super(nome, author, genre, duracao, lancamento);
@@ -14,4 +17,23 @@ public class EpPodcast extends Musica {
         votos.put(nome, voto);
     }
 
+    public void escreverDescricao(){
+
+        try{
+            var writter = new FileWriter(this.nome + ".txt");
+            writter.write(this.descrição);
+            writter.close();
+        } catch (IOException e){
+            e.printStackTrace();
+            System.out.println("Erro!\nNão foi possível escrever o arquivo");
+        }
+    }
+
+    public String getDescrição() {
+        return descrição;
+    }
+
+    public void setDescrição(String descrição) {
+        this.descrição = descrição;
+    }
 }
