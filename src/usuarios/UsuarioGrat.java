4f68f1daf3a85dba5d2a350fb09fca2be0155d60
ulcitrinitas <uli.citrinitas@gmail.com>
2025-09-16 16:55:24 -0300

    tocarMusica sobreescrito

---
+++ src/usuarios/UsuarioGrat.java
@@ -0,0 +1,25 @@
+package usuarios;
+
+import musicas.Musica;
+
+public class UsuarioGrat extends Usuario{
+
+    UsuarioGrat(String nome, String cpf, String email, String senha ) {
+        super(nome, cpf, email, senha);
+
+        this.pagante = false;
+        this.plano = "Gratuito";
+    }
+
+    @Override
+    void tocarMusica(Musica m){
+        System.out.println("Tocando " + m.getNome() + "....");
+        this.tocarAnuncio();
+        System.out.println("Tocando " + m.getNome() + "....");
+    }
+
+    void tocarAnuncio(){
+        System.out.println("Tocando Aunucio....");
+    }
+
+}
