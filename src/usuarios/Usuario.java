947fa6813b0ced2d9c0dba923c01b52582b4a947
ulcitrinitas <uli.citrinitas@gmail.com>
2025-09-16 15:23:42 -0300

    criados setters e getters

--- src/usuarios/Usuario.java
+++ src/usuarios/Usuario.java
@@ -1,6 +1,7 @@
 package usuarios;
 
 import java.security.MessageDigest;
+import java.time.Instant;
 import java.util.UUID;
 
 public class Usuario {
@@ -20,9 +21,61 @@ public class Usuario {
 
         this.cpf = "";
 
-        var idHash = UUID.randomUUID().toString();
-        this.id = idHash;
+        var msgHsh = new MessageDigest.getInstance("SHA-256");
+        msgHsh.update((nome + Instant.now()).getBytes());
+
+        this.id = new String(msgHsh.digest());
+
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 
 
 }
