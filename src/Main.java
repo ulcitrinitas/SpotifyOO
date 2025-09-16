eb129eb881d04de05bf7e2da36d9b502b564a0e7
ulcitrinitas <uli.citrinitas@gmail.com>
2025-09-16 18:07:40 -0300

    removidos testes

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,7 @@ import usuarios.*;
 public class Main {
     public static void main(String[] args) {
 
-        var m = new Musica("Hey, Soul Sister", "Train", "neo mellow", "2:17", "2010");
 
-        System.out.println("--------------------------------------------------------");
-        System.out.println(m.info());
-        System.out.println("--------------------------------------------------------");
-
-        var u = new Usuario("Ulisses", "xpto@email.com", "askmklsd", "user123");
-
-        System.out.println(u.getId());
-        System.out.println(u.getUsername());
 
     }
 }
