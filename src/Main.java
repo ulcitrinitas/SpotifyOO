9531698baaff7c400b9aac6624162276b9d35e36
ulcitrinitas <uli.citrinitas@gmail.com>
2025-09-16 14:08:02 -0300

    instancia um objeto musica

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import musicas.*;
+
 public class Main {
     public static void main(String[] args) {
+
+        var m = new Musica("Hey, Soul Sister", "Train", "neo mellow", "2:17", "2010");
+
+        System.out.println(m.info());
 
     }
 }
